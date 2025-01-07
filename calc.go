461b3cdff67ca352f3612f2de15022c40bf73ef6
nikita myazin <nikita.myazin@quadcode.com>
2025-01-07 21:13:50 +0200

    add docs

--- calc.go
+++ calc.go
@@ -1,3 +1,7 @@
+// Package super_duper_calculator is a super-duper calculator that I wrote while learning Go modules
+// ain't it fun!
+// You can learn about what it can do using incredible [mathisfun] website
+// [mathisfun]: https://www.mathisfun.com/numbers/addition.html
 package super_duper_calculator
 
 func Add(a, b int) int {
